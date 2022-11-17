c31aa42d7b7931033bad2ced8e948af3db59646d
Xasanboy <hasanbojabdurasulov309@gmail.com>
2022-11-17 21:25:28 +0500

    Hello

---
+++ 6 - Dars/TypeScript/index.ts
@@ -0,0 +1,16 @@
+function CheckNumber(a: number, b: number) {
+    let c: number = (a - b) / 2
+    console.log(c + 12.78)
+}
+// CheckNumber(5,7)
+
+
+function String_TO(x: string, i: number) {
+    console.log(`char at ${i} = ${x[i]} `)
+}
+// String_TO("Hello",3)
+
+function Length(firstPerson: string, Surname: string) {
+    console.log(firstPerson.length + Surname.length)
+}
+// Length("Khasanboy","Abdurasulov")
